d55947d3bb8df48d26a52d5bd54e2fbe3590126e
Drazuul <ivey.j97@gmail.com>
2026-08-04 23:45:27 -0500

    added math3d

---
+++ math3d.cpp
@@ -0,0 +1,26 @@
+/*
+    Vec3 (0,0,0)
+    Vec4 (0,0,0,0)
+    Mat4 { 
+          (0,0,0,0)
+          (0,0,0,0)
+          (0,0,0,0)
+          (0,0,0,0)    
+         }
+
+
+    vector operations
+        add
+        subtract
+        scale
+        dot
+        cross
+        mag
+        normalize
+
+    matrix operations
+        multiply
+
+*/
+
+
